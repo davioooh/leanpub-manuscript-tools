ee8d34fb0001da137ee27e182640e15b2a38efca
David Castelletti <d.castelletti@cogitoo.com>
2020-05-19 14:51:23 +0200

    A bit of (test) refactoring

---
+++ src/test/kotlin/com/davioooh/leanpub/ChaptersFunctionsTest.kt
@@ -0,0 +1,78 @@
+package com.davioooh.leanpub
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.io.TempDir
+import java.nio.file.Path
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+internal class ChaptersFunctionsTest {
+    private lateinit var testBook: TestBookFolder
+
+    private val expected3Chapters = listOf(
+        "ch01.txt",
+        "ch02.txt",
+        "ch03.txt"
+    )
+
+    private val expected3MdChapters = listOf(
+        "ch01.md",
+        "ch02.md",
+        "ch03.md"
+    )
+
+    private val expectedChaptersIndex = StringBuilder()
+        .apply { expected3Chapters.map { this.append("$it\r\n") } }
+        .toString()
+
+    @BeforeAll
+    fun setup(@TempDir testRootFolder: Path) {
+        testBook = TestBookFolder(testRootFolder)
+    }
+
+    @AfterEach
+    fun clearManuscriptFolder() {
+        testBook.deleteManuscriptFolder()
+    }
+
+    @Test
+    fun `should find 3 chapters in manuscript folder`() {
+        testBook.createManuscriptFolder().withFakeChapters(3)
+
+        val chapters = listAllChapters(testBook.bookRootFolder)
+
+        assertThat(chapters).hasSize(3)
+        assertThat(chapters.map { it.name }).isEqualTo(expected3Chapters)
+    }
+
+    @Test
+    fun `should generate Book-txt with 3 chapters`() {
+        testBook.createManuscriptFolder().withFakeChapters(3)
+
+        val bookTxt = generateBookTxtFromChapters(testBook.bookRootFolder, expected3Chapters)
+
+        assertThat(bookTxt.readText()).isEqualTo(expectedChaptersIndex)
+    }
+
+    @Test
+    fun `should convert TXT chapters to MD`() {
+        testBook.createManuscriptFolder().withFakeChapters(3)
+
+        val mdChapters = convertTxtChaptersToMd(testBook.bookRootFolder)
+
+        assertThat(mdChapters.map { it.name }).isEqualTo(expected3MdChapters)
+    }
+
+    @Test
+    fun `should convert MD chapters to TXT`() {
+        testBook.createManuscriptFolder().withFakeChapters(3, chaptersExt = MD_EXT)
+
+        val txtChapters = convertMdChaptersToTxt(testBook.bookRootFolder)
+
+        assertThat(txtChapters.map { it.name }).isEqualTo(expected3Chapters)
+    }
+
+}
---
+++ src/test/kotlin/com/davioooh/leanpub/TestBookFolder.kt
@@ -0,0 +1,43 @@
+package com.davioooh.leanpub
+
+import java.nio.file.Path
+
+class TestBookFolder(val bookRootFolder: Path) {
+    private val manuscriptFolder = bookRootFolder.resolve(MANUSCRIPT_FOLDER)
+
+    fun createManuscriptFolder(): TestBookFolder {
+        manuscriptFolder.apply { toFile().mkdir() }
+        return TestBookFolder(bookRootFolder)
+    }
+
+    fun withFakeChapters(numberOfChapters: Int, chaptersExt: String = TXT_EXT): TestBookFolder {
+        repeat(numberOfChapters) {
+            createFakeChapterFile(it + 1, chaptersExt)
+        }
+        return this
+    }
+
+    fun deleteManuscriptFolder(): TestBookFolder {
+        manuscriptFolder.toFile().deleteRecursively()
+        return this
+    }
+
+    private fun createFakeChapterFile(chapterNum: Int, fileExt: String): Unit =
+        manuscriptFolder
+            .resolve(testFileNameFrom(chapterNum, fileExt))
+            .toFile()
+            .apply { createNewFile() }
+            .let {
+                it.printWriter().use { out ->
+                    out.println("# Chapter $chapterNum")
+                    out.println()
+                    out.println("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis mi nec risus sollicitudin finibus. Maecenas nec gravida mauris. Maecenas sit amet quam ac tellus placerat molestie. Proin vestibulum sapien feugiat, vulputate tortor eget, rhoncus eros. Nam non elit eget mauris porttitor sollicitudin. Aenean ullamcorper, magna at pharetra pulvinar, sapien magna pulvinar tortor, ut dignissim purus nulla ac purus. Morbi in risus sit amet ipsum sodales feugiat sit amet in arcu. Integer at feugiat sem, quis tincidunt risus. Vestibulum tincidunt sem et dignissim molestie. Curabitur fringilla magna sit amet vehicula bibendum. Phasellus blandit auctor justo, at aliquet magna mollis ac. Curabitur posuere pharetra tristique. Vestibulum tristique mi id velit gravida condimentum. Donec aliquam congue quam, a luctus leo pharetra a. Aliquam faucibus sollicitudin massa, ut interdum velit commodo id. Duis eros ipsum, faucibus vitae dapibus at, ornare in nisi.")
+                    out.println()
+                    out.println("Nullam tellus neque, consequat quis eleifend a, dignissim dignissim ipsum. Ut placerat dignissim interdum. Nam dignissim sagittis interdum. Cras tortor erat, volutpat ut sagittis in, tincidunt eget mauris. Quisque a felis sed urna sollicitudin ornare commodo eu libero. Morbi luctus tristique ante nec pulvinar. Mauris interdum ante et velit convallis volutpat. Sed rutrum lacus et ligula maximus ultrices. Nulla quis dignissim metus, ac euismod leo. Sed auctor lorem et eleifend ultricies. Nullam suscipit tellus eget molestie lobortis. Morbi viverra ipsum sit amet molestie eleifend.")
+                }
+            }
+
+    // TODO optimize file name creation
+    private fun testFileNameFrom(chapterNum: Int, fileExt: String) =
+        "${CHAPTER_PREFIX}0$chapterNum.$fileExt"
+}
