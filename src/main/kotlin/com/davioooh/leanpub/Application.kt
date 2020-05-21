79569dd79f26a72cb911e8a11f9c6f85c8e5e569
David Castelletti <d.castelletti@cogitoo.com>
2020-05-21 17:44:55 +0200

    Add first CLI command: chapters (listing)

--- src/main/kotlin/com/davioooh/leanpub/Application.kt
+++ src/main/kotlin/com/davioooh/leanpub/Application.kt
@@ -1,5 +1,27 @@
 package com.davioooh.leanpub
 
-fun main(args: Array<String>) {
-    println("Hello, World")
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.NoOpCliktCommand
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.path
+import java.nio.file.Path
+
+class LPTools : NoOpCliktCommand()
+
+class Chapters : CliktCommand(help = "List all chapters in manuscript folder") {
+    val bookFolder by option(
+            "-bf", "--book-folder",
+            help = "The root folder of the book (containing the manuscript/ folder)").path()
+            .default(Path.of("."))
+
+    override fun run() {
+        listAllChapters(bookFolder).map { it.name }.forEach { echo(it) }
+    }
 }
+
+fun main(args: Array<String>) =
+        LPTools()
+                .subcommands(Chapters())
+                .main(args)
---
+++ src/test/kotlin/com/davioooh/leanpub/LPToolsTest.kt
@@ -0,0 +1,11 @@
+package com.davioooh.leanpub
+
+import com.github.ajalt.clikt.core.subcommands
+import org.junit.jupiter.api.Test
+
+internal class LPToolsTest {
+    @Test
+    fun test() {
+        LPTools().subcommands(Chapters()).parse(arrayOf("chapters", "-bf=test\\path"))
+    }
+}
