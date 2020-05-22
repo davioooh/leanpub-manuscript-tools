54c30b49b4fe9322e0add9eebf922882e795c266
David Castelletti <d.castelletti@cogitoo.com>
2020-05-22 11:54:09 +0200

    Update "chapter list" command

--- src/main/kotlin/com/davioooh/leanpub/Application.kt
+++ src/main/kotlin/com/davioooh/leanpub/Application.kt
@@ -10,8 +10,14 @@ import java.nio.file.Path
 
 class LPTools : NoOpCliktCommand()
 
-class Chapters : CliktCommand(help = "List all chapters in manuscript folder") {
-    val bookFolder by option(
+class Chapters : NoOpCliktCommand(){
+    override fun aliases(): Map<String, List<String>> = mapOf(
+            "l" to listOf("list")
+    )
+}
+
+class ChList : CliktCommand(name = "list", help = "List all chapters in manuscript folder") {
+    private val bookFolder by option(
             "-bf", "--book-folder",
             help = "The root folder of the book (containing the manuscript/ folder)").path()
             .default(Path.of("."))
@@ -23,5 +29,5 @@ class Chapters : CliktCommand(help = "List all chapters in manuscript folder") {
 
 fun main(args: Array<String>) =
         LPTools()
-                .subcommands(Chapters())
+                .subcommands(Chapters().subcommands(ChList()))
                 .main(args)
