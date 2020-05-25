df30eec7402f4977e4a6cc82cb30be73b0c3ba6a
David Castelletti <d.castelletti@cogitoo.com>
2020-05-25 11:29:51 +0200

    Update LPTools logic + tests

--- src/main/kotlin/com/davioooh/leanpub/Application.kt
+++ src/main/kotlin/com/davioooh/leanpub/Application.kt
@@ -2,10 +2,31 @@ package com.davioooh.leanpub
 
 import com.davioooh.leanpub.commands.Chapters
 import com.davioooh.leanpub.commands.Chapters.GetList
-import com.github.ajalt.clikt.core.NoOpCliktCommand
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.findOrSetObject
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.path
+import java.nio.file.Path
 
-class LPTools : NoOpCliktCommand()
+class LPTools : CliktCommand(name = APP_NAME, help = APP_HELP_MSG, epilog = APP_EPILOG_MSG) {
+    private val bookFolder by option("-bf", "--book-folder", help = BOOK_FOLDER_OPT_HELP_MSG).path()
+            .default(Path.of("."))
+    private val config by findOrSetObject { Config() }
+    override fun run() {
+        config.bookFolder = bookFolder
+    }
+
+    companion object {
+        const val APP_NAME = "lptools"
+        const val APP_HELP_MSG = "Command line tools for Leanpub authors."
+        const val APP_EPILOG_MSG = "Run '$APP_NAME COMMAND --help' for more information on a command."
+        const val BOOK_FOLDER_OPT_HELP_MSG = "The root folder of the book (containing the manuscript folder)"
+    }
+
+    data class Config(var bookFolder: Path? = null)
+}
 
 fun main(args: Array<String>) =
         LPTools()
--- src/test/kotlin/com/davioooh/leanpub/LPToolsTest.kt
+++ src/test/kotlin/com/davioooh/leanpub/LPToolsTest.kt
@@ -1,12 +1,39 @@
 package com.davioooh.leanpub
 
-import com.davioooh.leanpub.commands.Chapters
+import com.davioooh.leanpub.LPTools.Config
+import com.github.ajalt.clikt.core.NoOpCliktCommand
+import com.github.ajalt.clikt.core.PrintHelpMessage
 import com.github.ajalt.clikt.core.subcommands
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import java.nio.file.Path
 
 internal class LPToolsTest {
+
+    private val lpTools = LPTools().subcommands(DummyCommand())
+
     @Test
-    fun test() {
-        LPTools().subcommands(Chapters()).parse(arrayOf("chapters", "-bf=test\\path"))
+    fun `running with not child command should print help`() {
+        assertThatThrownBy {
+            lpTools.parse(arrayOf())
+        }.isInstanceOf(PrintHelpMessage::class.java)
     }
+
+    @Test
+    fun `default book folder should be set in command context`() {
+        lpTools.parse(arrayOf("fake-cmd"))
+        val config = lpTools.currentContext.findObject<Config>()!!
+        assertThat(config.bookFolder).isEqualTo(Path.of("."))
+    }
+
+    @Test
+    fun `provided book folder should be set in command context`() {
+        lpTools.parse(arrayOf("-bf=test/path", "fake-cmd"))
+        val config = lpTools.currentContext.findObject<Config>()!!
+        assertThat(config.bookFolder).isEqualTo(Path.of("test/path"))
+    }
+
+    class DummyCommand : NoOpCliktCommand(name = "fake-cmd")
+
 }
