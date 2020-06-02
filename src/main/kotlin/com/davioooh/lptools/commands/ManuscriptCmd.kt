c327e8bace0ae1e069baae7c81c27ad5712980b5
David Castelletti <d.castelletti@cogitoo.com>
2020-06-03 00:51:38 +0200

    Fix ManuscriptCmd

--- src/main/kotlin/com/davioooh/lptools/commands/ManuscriptCmd.kt
+++ src/main/kotlin/com/davioooh/lptools/commands/ManuscriptCmd.kt
@@ -3,6 +3,7 @@ package com.davioooh.lptools.commands
 import com.davioooh.lptools.LPTools
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.NoOpCliktCommand
+import com.github.ajalt.clikt.core.UsageError
 import com.github.ajalt.clikt.core.requireObject
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
@@ -12,27 +13,39 @@ class ManuscriptCmd : NoOpCliktCommand(name = MANUSCRIPT_CMD_NAME) {
             "gn" to listOf(GENERATE_CMD_NAME)
     )
 
-    class Generate(val listChapterFilesFun: ListChapterFilesFun, val generateBookTxtFun: GenerateBookTxtFun) :
-            CliktCommand(name = GENERATE_CMD_NAME, help = GENERATE_HELP_MSG) {
+    class Generate(val listChapterFiles: ListChapterFilesFun, val generateBookTxt: GenerateBookTxtFun) :
+            CliktCommand(name = GENERATE_CMD_NAME, help = CMD_HELP_MSG) {
         private val config by requireObject<LPTools.Config>()
         private val bookTxt by option("--book-txt", "-b", help = BOOK_TXT_OPT_HELP_MSG).flag()
 
         override fun run() {
-            if (bookTxt) {
-                // TODO what if I want to pass file names from console?
-                val bookRootFolder = config.bookFolder!!
-                val fileNames = listChapterFilesFun(bookRootFolder).map { it.name }
-                val bookTxt = generateBookTxtFun(bookRootFolder, fileNames)
-                echo("Generated: " + bookTxt.relativeTo(bookRootFolder.toFile()))
+            when {
+                bookTxt -> {
+                    // TODO what if I want to pass file names from console?
+                    val bookRootFolder = config.bookFolder!!
+                    val fileNames = listChapterFiles(bookRootFolder).map { it.name }
+                    if (fileNames.isNotEmpty()) {
+                        val bookTxt = generateBookTxt(bookRootFolder, fileNames)
+                        echo("Generated: $bookTxt")
+                    } else {
+                        echo("Book.txt not generated: no chapter files found in manuscript folder.")
+                    }
+                }
+                else -> {
+                    throw UsageError("At least one option is required.")
+                }
             }
+        }
+
+        companion object {
+            const val CMD_HELP_MSG = "" // TODO some helpful message...
+            const val BOOK_TXT_OPT_HELP_MSG = "Generate Book.txt file."
         }
     }
 
     companion object {
         const val MANUSCRIPT_CMD_NAME = "manuscript"
         const val GENERATE_CMD_NAME = "generate"
-        const val GENERATE_HELP_MSG = "" // TODO some helpful message...
-        const val BOOK_TXT_OPT_HELP_MSG = "Generate Book.txt file."
     }
 
 }
