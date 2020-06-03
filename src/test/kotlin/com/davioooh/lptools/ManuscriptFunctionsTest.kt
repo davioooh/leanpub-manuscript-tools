2aa2d89b0a91f75a7f5c46d628de44d5c879ab75
David Castelletti <d.castelletti@cogitoo.com>
2020-06-03 20:57:57 +0200

    Rename ChaptersFunctions to ManuscriptFunctions

--- src/test/kotlin/com/davioooh/lptools/ManuscriptFunctionsTest.kt
+++ src/test/kotlin/com/davioooh/lptools/ManuscriptFunctionsTest.kt
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.io.TempDir
 import java.nio.file.Path
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-internal class ChaptersFunctionsTest {
+internal class ManuscriptFunctionsTest {
     private lateinit var testBook: TestBookFolder
 
     private val expected3TxtFiles = fakeTxtFileNamesList
