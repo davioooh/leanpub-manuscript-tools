3163af2bb9a9aa0de30d81e0d6ee0af9b9813bce
David Castelletti <d.castelletti@cogitoo.com>
2020-06-15 17:26:00 +0200

    Improve tests

--- src/test/kotlin/com/davioooh/lptools/LPToolsTest.kt
+++ src/test/kotlin/com/davioooh/lptools/LPToolsTest.kt
@@ -1,11 +1,9 @@
 package com.davioooh.lptools
 
 import com.davioooh.lptools.LPTools.Config
+import com.davioooh.lptools.commands.ResolveManuscriptPathFun
 import com.davioooh.lptools.commons.*
-import com.github.ajalt.clikt.core.PrintHelpMessage
-import com.github.ajalt.clikt.core.UsageError
-import com.github.ajalt.clikt.core.context
-import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.core.*
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
@@ -13,14 +11,6 @@ import org.junit.jupiter.api.Test
 import java.nio.file.Path
 
 internal class LPToolsTest {
-
-    private val lpTools = LPTools { TEST_MANUSCRIPT_PATH }
-            .apply { context { console = TestConsole } }
-            .subcommands(FakeCommand)
-    private val lpToolsWithInvalidBookFolder = LPTools { null }
-            .apply { context { console = TestConsole } }
-            .subcommands(FakeCommand)
-
 
     @BeforeEach
     fun setup() {
@@ -30,13 +20,13 @@ internal class LPToolsTest {
     @Test
     fun `running with no child command or option should print help`() {
         assertThatThrownBy {
-            lpTools.parse(arrayOf())
+            lpToolsWith(FakeCommand).parse(arrayOf())
         }.isInstanceOf(PrintHelpMessage::class.java)
     }
 
     @Test
     fun `running with --version option should print version number`() {
-        lpTools.parse(arrayOf("--version"))
+        lpToolsWith(FakeCommand).parse(arrayOf("--version"))
 
         assertThat(TestConsole.output).containsPattern("^v.*$")
     }
@@ -44,12 +34,14 @@ internal class LPToolsTest {
     @Test
     fun `running with no child command should print help -- also when book path is not valid`() {
         assertThatThrownBy {
-            lpToolsWithInvalidBookFolder.parse(arrayOf())
+            lpToolsWith(FakeCommand) { null }.parse(arrayOf())
         }.isInstanceOf(PrintHelpMessage::class.java)
     }
 
     @Test
     fun `default book folder should be set in command context`() {
+        val lpTools = lpToolsWith(FakeCommand)
+
         lpTools.parse(arrayOf(FAKE_CMD_NAME))
 
         val config = lpTools.currentContext.findObject<Config>()!!
@@ -58,6 +50,8 @@ internal class LPToolsTest {
 
     @Test
     fun `provided book folder should be set in command context`() {
+        val lpTools = lpToolsWith(FakeCommand)
+
         lpTools.parse(arrayOf("-bf=$TEST_BOOK_URL", FAKE_CMD_NAME))
 
         val config = lpTools.currentContext.findObject<Config>()!!
@@ -67,8 +61,15 @@ internal class LPToolsTest {
     @Test
     fun `should print out error if book folder is not valid and a child cmd is invoked`() {
         assertThatThrownBy {
-            lpToolsWithInvalidBookFolder.parse(arrayOf("-bf=$TEST_BOOK_URL", FAKE_CMD_NAME))
+            lpToolsWith(FakeCommand) { null }.parse(arrayOf("-bf=$TEST_BOOK_URL", FAKE_CMD_NAME))
         }.isInstanceOf(UsageError::class.java)
     }
+
+    private fun lpToolsWith(
+            subCmd: CliktCommand,
+            resolveManuscriptPath: ResolveManuscriptPathFun = { TEST_MANUSCRIPT_PATH }
+    ) = LPTools(resolveManuscriptPath)
+            .apply { context { console = TestConsole } }
+            .subcommands(subCmd)
 
 }
